1ff181c99eb952f49af921e8a2f3c21d7a4956c7
Syméon ROUGEVIN <symeon@oclocher.fr>
2024-04-19 16:19:21 +0200

    changes

--- src/main/java/labJunit/app/CompteBancaire.java
+++ src/main/java/labJunit/app/CompteBancaire.java
@@ -9,14 +9,14 @@ public class CompteBancaire {
 	
 	public double debiterSolde(double montant) throws IllegalArgumentException
 	{
-    if (montant < 0) throw new IllegalArgumentException("Montant négatif.");
+    if (montant < 0) throw new IllegalArgumentException("Le montant doit être positif.");
 		solde -= montant;
     return solde;
 	}
 	
 	public double crediterSolde(double montant) throws IllegalArgumentException
 	{
-    if (montant < 0) throw new IllegalArgumentException("Montant négatif.");
+    if (montant < 0) throw new IllegalArgumentException("Le montant doit être positif.");
 		solde += montant;
     return solde;
 	}
